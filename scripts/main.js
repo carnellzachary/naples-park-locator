7aa54cfd68953ebc07529fe673b6f52a033a1019
carnellzachary <113737991+carnellzachary@users.noreply.github.com>
2022-12-13 13:20:21 -0500

    Final Commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
-let desc = document.querySelector('#parkDescription');
-let parkDetails = document.querySelector('#parkDetails');
-let selectedCriteriaBox = document.querySelector('#selectedCriteria');
-let naplesMap = L.map('naplesMap').setView([26.193813, -81.705949], 12.33);
+let naplesMap = L.map('naplesMap').setView([26.194720, -81.696345], 12.33);
 
+// jsMediaQuery code from W3Schools: https://www.w3schools.com/howto/howto_js_media_queries.asp
+function jsMediaQuery(x) {
+  if (x.matches && naplesMap.getZoom() == 12 ) { // If media query matches
+    // Changes view for phone
+    naplesMap.zoomOut();
+  }
+}
+
+let mqPhone = window.matchMedia("(max-width: 750px)");
+jsMediaQuery(mqPhone); // Call listener function at run time
+mqPhone.addListener(jsMediaQuery); // Attach listener function on state changes
 
 // MapBox theme and attribution for Leaflet map
 L.tileLayer('https://api.mapbox.com/styles/v1/zcarnell/clan79yjz002i15pfcn7imdqz/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiemNhcm5lbGwiLCJhIjoiY2xhbjZ6czZsMDVraTNvcGQ1b2h6Zjc2bCJ9.v-shyASMRnY1z_N6vrlt2w', {
@@ -11,16 +19,15 @@ L.tileLayer('https://api.mapbox.com/styles/v1/zcarnell/clan79yjz002i15pfcn7imdqz
 }).addTo(naplesMap);
 
 // Populates Leaflet map with markers
-for (let i = 0; i < places.length; i++) {
-  places[i].marker = L.marker([places[i].lat, places[i].long], {
-    alt: places[i].id
+for (let i = 0; i < allParks.length; i++) {
+  allParks[i].marker = L.marker([allParks[i].lat, allParks[i].long], {
+    alt: allParks[i].id
   }).on('click', markerOnClick);
 
-  places[i].marker.addTo(naplesMap);
+  allParks[i].marker.bindPopup('<h3><a href="' + allParks[i].agencyurl + '">' + allParks[i].placeName + '</a></h3>' + '<p>"' + allParks[i].desc + '" (<a href="https://hub-collierbcc.opendata.arcgis.com/datasets/CollierBCC::parks/explore?location=26.147654%2C-81.590115%2C10.92&showTable=true">Collier County GIS</a>).</p><p>Open: ' + allParks[i].operdays + ', ' + allParks[i].operhours + '</p>').addTo(naplesMap);
 }
 
 function markerOnClick() {
-
   let allMarkers = document.querySelectorAll(".leaflet-marker-icon");
 
   // removes all huechange classes before adding a new one
@@ -33,28 +40,12 @@ function markerOnClick() {
   let park_id = this.options.alt;
 
   // get the index of the park object
-  let i = places.findIndex((park, i) => {
+  let i = allParks.findIndex((park, i) => {
     if (park.id === park_id) {
       // How to change Leaflet marker color, presented by sil (IE not supported): https://hungchienhsiang.medium.com/super-simple-way-to-change-leaflet-marker-color-7efd0996c797
       this._icon.classList.add("huechange");
-      return true
     }
   });
-
-  // write descrip for that state into external DIV
-  desc.innerHTML = '<h3><a href="' + places[i].agencyurl + '">' + places[i].placeName + '</a></h3>' + '<p>"' + places[i].desc + '" (<a href="https://hub-collierbcc.opendata.arcgis.com/datasets/CollierBCC::parks/explore?location=26.147654%2C-81.590115%2C10.92&showTable=true">Collier County GIS</a>).</p>';
-
-  // Transition header to show description
-  document.querySelector('#mainContainer').classList = 'mainContainerShowDesc';
-
-  // Check if has parking
-  if (places[i].numparking > 0) {
-    parkingAmount = 'Yes (' + places[i].numparking + ')';
-  } else {
-    parkingAmount = 'No';
-  }
-
-  parkDetails.innerHTML = '<ul><li>Parking: ' + parkingAmount + '</li><li>Open: 7 days a week (Daylight)</li></ul>';
 }
 
 function parkOptionClick(event) {
@@ -68,10 +59,6 @@ function parkOptionClick(event) {
     parkOption.classList.add('selectedOption');
   }
 
-  // Add option to selectedCriteria
-  // selectedCriteriaBox.innerHTML += '<div>' + event.currentTarget.innerHTML + '</div>';
-  console.log(parkOption.getAttribute('value'));
-
   repopulateMap();
 }
 
@@ -80,14 +67,14 @@ function repopulateMap() {
   let selectedParkDivs = document.querySelectorAll('.selectedOption');
 
   if (selectedParkDivs.length == 0) {
-    parksByCriteria = places;
+    parksByCriteria = allParks;
   } else {
     // Filter parks by each criteria selected
     selectedParkDivs.forEach(function(i) {
-      switch (i.getAttribute('value')) {
+      switch (i.getAttribute('id')) {
         case 'food':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.food === "Yes"
             });
           } else {
@@ -98,7 +85,7 @@ function repopulateMap() {
           break;
         case 'bathroom':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.restroom === "Yes"
             });
           } else {
@@ -109,7 +96,7 @@ function repopulateMap() {
           break;
         case 'picnic':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.picnic === "Yes"
             });
           } else {
@@ -120,7 +107,7 @@ function repopulateMap() {
           break;
         case 'parking':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.numparking > 0
             });
           } else {
@@ -131,7 +118,7 @@ function repopulateMap() {
           break;
         case 'handicapac':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.handicapac === "Yes"
             });
           } else {
@@ -142,7 +129,7 @@ function repopulateMap() {
           break;
         case 'baseball':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.baseball === "Yes"
             });
           } else {
@@ -153,7 +140,7 @@ function repopulateMap() {
           break;
         case 'basketball':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.basketball === "Yes"
             });
           } else {
@@ -164,7 +151,7 @@ function repopulateMap() {
           break;
         case 'pickleball':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.pickleball === "Yes"
             });
           } else {
@@ -175,7 +162,7 @@ function repopulateMap() {
           break;
         case 'soccer':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.soccer === "Yes"
             });
           } else {
@@ -186,7 +173,7 @@ function repopulateMap() {
           break;
         case 'tennis':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.tennis === "Yes"
             });
           } else {
@@ -197,7 +184,7 @@ function repopulateMap() {
           break;
         case 'volleyball':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.volleyball === "Yes"
             });
           } else {
@@ -208,7 +195,7 @@ function repopulateMap() {
           break;
         case 'fishing':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.fishing === "Yes"
             });
           } else {
@@ -219,7 +206,7 @@ function repopulateMap() {
           break;
         case 'boating':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.boating === "Yes"
             });
           } else {
@@ -230,7 +217,7 @@ function repopulateMap() {
           break;
         case 'swimming':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.swimming === "Yes"
             });
           } else {
@@ -241,7 +228,7 @@ function repopulateMap() {
           break;
         case 'waterfeature':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.waterfeature === "Yes"
             });
           } else {
@@ -252,7 +239,7 @@ function repopulateMap() {
           break;
         case 'beach':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.beach === "Yes"
             });
           } else {
@@ -263,7 +250,7 @@ function repopulateMap() {
           break;
         case 'playground':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.playground === "Yes"
             });
           } else {
@@ -274,7 +261,7 @@ function repopulateMap() {
           break;
         case 'hiking':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.hiking === "Yes"
             });
           } else {
@@ -285,7 +272,7 @@ function repopulateMap() {
           break;
         case 'biking':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.bike === "Yes"
             });
           } else {
@@ -296,7 +283,7 @@ function repopulateMap() {
           break;
         case 'fitness':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.fitness === "Yes"
             });
           } else {
@@ -307,7 +294,7 @@ function repopulateMap() {
           break;
         case 'shuffleboard':
           if (parksByCriteria.length == 0) {
-            parksByCriteria = places.filter(obj => {
+            parksByCriteria = allParks.filter(obj => {
               return obj.shuffleboard === "Yes"
             });
           } else {
@@ -320,15 +307,9 @@ function repopulateMap() {
     });
   }
 
-  console.log(parksByCriteria);
-
-  // Clear description
-  desc.innerHTML = '';
-  parkDetails.innerHTML = '';
-
   // Remove markers presently on map
-  for (let i = 0; i < places.length; i++) {
-    naplesMap.removeLayer(places[i].marker);
+  for (let i = 0; i < allParks.length; i++) {
+    naplesMap.removeLayer(allParks[i].marker);
   }
 
   // Remove markers presently on map
@@ -342,13 +323,6 @@ function repopulateMap() {
       alt: parksByCriteria[i].id
     }).on('click', markerOnClick);
 
-    parksByCriteria[i].marker.addTo(naplesMap);
+    parksByCriteria[i].marker.bindPopup('<h3><a href="' + allParks[i].agencyurl + '">' + allParks[i].placeName + '</a></h3>' + '<p>"' + allParks[i].desc + '" (<a href="https://hub-collierbcc.opendata.arcgis.com/datasets/CollierBCC::parks/explore?location=26.147654%2C-81.590115%2C10.92&showTable=true">Collier County GIS</a>).</p><p>Open: ' + allParks[i].operdays + ', ' + allParks[i].operhours + '</p>').addTo(naplesMap);
   }
 }
-
-// Maybe do marker clusters: https://stackoverflow.com/questions/49333263/how-to-use-leaflet-markerclustergroup
-
-// Old desc bind to marker popup
-//for (let i = 0; i < places.length; i++) {
-// L.marker([places[i].lat, places[i].long]).bindPopup('<h3><a href="' + places[i].agencyurl + '">' + places[i].placeName + '</a></h3>' + '<p>"' + places[i].desc + '" (<a href="https://hub-collierbcc.opendata.arcgis.com/datasets/CollierBCC::parks/explore?location=26.147654%2C-81.590115%2C10.92&showTable=true">Collier County GIS</a>).</p>').addTo(naplesMap);
-// }
