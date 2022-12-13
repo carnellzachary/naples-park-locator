7c4140bc4e69ee8c93292322d3a3bc371d339137
carnellzachary <113737991+carnellzachary@users.noreply.github.com>
2022-12-12 22:33:59 -0500

    secondToLast Commit
    Basic functionality and layout is done. Just need to make it responsive and then polish a few UI things

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 let desc = document.querySelector('#parkDescription');
 let parkDetails = document.querySelector('#parkDetails');
 let selectedCriteriaBox = document.querySelector('#selectedCriteria');
-let naplesMap = L.map('naplesMap').setView([26.193813, -81.705949], 11.33);
+let naplesMap = L.map('naplesMap').setView([26.193813, -81.705949], 12.33);
 
 
 // MapBox theme and attribution for Leaflet map
@@ -14,7 +14,9 @@ L.tileLayer('https://api.mapbox.com/styles/v1/zcarnell/clan79yjz002i15pfcn7imdqz
 for (let i = 0; i < places.length; i++) {
   places[i].marker = L.marker([places[i].lat, places[i].long], {
     alt: places[i].id
-  }).on('click', markerOnClick).addTo(naplesMap);
+  }).on('click', markerOnClick);
+
+  places[i].marker.addTo(naplesMap);
 }
 
 function markerOnClick() {
@@ -42,6 +44,9 @@ function markerOnClick() {
   // write descrip for that state into external DIV
   desc.innerHTML = '<h3><a href="' + places[i].agencyurl + '">' + places[i].placeName + '</a></h3>' + '<p>"' + places[i].desc + '" (<a href="https://hub-collierbcc.opendata.arcgis.com/datasets/CollierBCC::parks/explore?location=26.147654%2C-81.590115%2C10.92&showTable=true">Collier County GIS</a>).</p>';
 
+  // Transition header to show description
+  document.querySelector('#mainContainer').classList = 'mainContainerShowDesc';
+
   // Check if has parking
   if (places[i].numparking > 0) {
     parkingAmount = 'Yes (' + places[i].numparking + ')';
@@ -55,268 +60,290 @@ function markerOnClick() {
 function parkOptionClick(event) {
   // Selected option styling
   let parkOption = event.currentTarget;
+
+  // Maybe just use toggle?
   if (parkOption.classList.contains('selectedOption')) {
     parkOption.classList.remove('selectedOption');
   } else {
-    // Add styling to selevted div
     parkOption.classList.add('selectedOption');
   }
 
   // Add option to selectedCriteria
-  selectedCriteriaBox.innerHTML += '<div>' + event.currentTarget.innerHTML + '</div>';
+  // selectedCriteriaBox.innerHTML += '<div>' + event.currentTarget.innerHTML + '</div>';
   console.log(parkOption.getAttribute('value'));
+
+  repopulateMap();
 }
 
 function repopulateMap() {
   parksByCriteria = [];
   let selectedParkDivs = document.querySelectorAll('.selectedOption');
-  selectedParkDivs.forEach(function(i) {
-    // switch-case for each button that defines certain varibles. Filter places array by those varibles
-    switch (i.getAttribute('value')) {
-      case 'food':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.food === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.food === "Yes"
-          });
-        }
-        break;
-      case 'bathroom':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.restroom === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.restroom === "Yes"
-          });
-        }
-        break;
-      case 'picnic':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.picnic === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.picnic === "Yes"
-          });
-        }
-        break;
-      case 'parking':
-        // Do this later after adding parking attribute to parent array
-        parkingPref = "Yes";
-        break;
-      case 'handicapac':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.handicapac === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.handicapac === "Yes"
-          });
-        }
-        break;
-      case 'baseball':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.baseball === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.baseball === "Yes"
-          });
-        }
-        break;
-      case 'basketball':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.basketball === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.basketball === "Yes"
-          });
-        }
-        break;
-      case 'pickleball':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.pickleball === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.pickleball === "Yes"
-          });
-        }
-        break;
-      case 'soccer':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.soccer === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.soccer === "Yes"
-          });
-        }
-        break;
-      case 'tennis':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.tennis === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.tennis === "Yes"
-          });
-        }
-        break;
-      case 'volleyball':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.volleyball === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.volleyball === "Yes"
-          });
-        }
-        break;
-      case 'fishing':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.fishing === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.fishing === "Yes"
-          });
-        }
-        break;
-      case 'boating':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.boating === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.boating === "Yes"
-          });
-        }
-        break;
-      case 'swimming':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.swimming === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.swimming === "Yes"
-          });
-        }
-        break;
-      case 'waterfeature':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.waterfeature === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.waterfeature === "Yes"
-          });
-        }
-        break;
-      case 'beach':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.beach === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.beach === "Yes"
-          });
-        }
-        break;
-      case 'playground':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.playground === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.playground === "Yes"
-          });
-        }
-        break;
-      case 'hiking':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.hiking === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.hiking === "Yes"
-          });
-        }
-        break;
-      case 'biking':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.bike === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.bikingPref === "Yes"
-          });
-        }
-        break;
-      case 'fitness':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.fitness === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.fitness === "Yes"
-          });
-        }
-        break;
-      case 'shuffleboard':
-        if (parksByCriteria.length == 0) {
-          parksByCriteria = places.filter(obj => {
-            return obj.shuffleboard === "Yes"
-          });
-        } else {
-          parksByCriteria = parksByCriteria.filter(obj => {
-            return obj.shuffleboard === "Yes"
-          });
-        }
-        break;
-    }
-  });
+
+  if (selectedParkDivs.length == 0) {
+    parksByCriteria = places;
+  } else {
+    // Filter parks by each criteria selected
+    selectedParkDivs.forEach(function(i) {
+      switch (i.getAttribute('value')) {
+        case 'food':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.food === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.food === "Yes"
+            });
+          }
+          break;
+        case 'bathroom':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.restroom === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.restroom === "Yes"
+            });
+          }
+          break;
+        case 'picnic':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.picnic === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.picnic === "Yes"
+            });
+          }
+          break;
+        case 'parking':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.numparking > 0
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.numparking > 0
+            });
+          }
+          break;
+        case 'handicapac':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.handicapac === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.handicapac === "Yes"
+            });
+          }
+          break;
+        case 'baseball':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.baseball === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.baseball === "Yes"
+            });
+          }
+          break;
+        case 'basketball':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.basketball === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.basketball === "Yes"
+            });
+          }
+          break;
+        case 'pickleball':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.pickleball === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.pickleball === "Yes"
+            });
+          }
+          break;
+        case 'soccer':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.soccer === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.soccer === "Yes"
+            });
+          }
+          break;
+        case 'tennis':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.tennis === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.tennis === "Yes"
+            });
+          }
+          break;
+        case 'volleyball':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.volleyball === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.volleyball === "Yes"
+            });
+          }
+          break;
+        case 'fishing':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.fishing === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.fishing === "Yes"
+            });
+          }
+          break;
+        case 'boating':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.boating === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.boating === "Yes"
+            });
+          }
+          break;
+        case 'swimming':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.swimming === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.swimming === "Yes"
+            });
+          }
+          break;
+        case 'waterfeature':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.waterfeature === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.waterfeature === "Yes"
+            });
+          }
+          break;
+        case 'beach':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.beach === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.beach === "Yes"
+            });
+          }
+          break;
+        case 'playground':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.playground === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.playground === "Yes"
+            });
+          }
+          break;
+        case 'hiking':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.hiking === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.hiking === "Yes"
+            });
+          }
+          break;
+        case 'biking':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.bike === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.bikingPref === "Yes"
+            });
+          }
+          break;
+        case 'fitness':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.fitness === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.fitness === "Yes"
+            });
+          }
+          break;
+        case 'shuffleboard':
+          if (parksByCriteria.length == 0) {
+            parksByCriteria = places.filter(obj => {
+              return obj.shuffleboard === "Yes"
+            });
+          } else {
+            parksByCriteria = parksByCriteria.filter(obj => {
+              return obj.shuffleboard === "Yes"
+            });
+          }
+          break;
+      }
+    });
+  }
 
   console.log(parksByCriteria);
 
+  // Clear description
+  desc.innerHTML = '';
+  parkDetails.innerHTML = '';
 
+  // Remove markers presently on map
   for (let i = 0; i < places.length; i++) {
     naplesMap.removeLayer(places[i].marker);
   }
 
+  // Remove markers presently on map
   for (let i = 0; i < parksByCriteria.length; i++) {
     naplesMap.removeLayer(parksByCriteria[i].marker);
   }
 
+  // Add the newly filtered markers
   for (let i = 0; i < parksByCriteria.length; i++) {
     parksByCriteria[i].marker = L.marker([parksByCriteria[i].lat, parksByCriteria[i].long], {
       alt: parksByCriteria[i].id
-    }).on('click', markerOnClick).addTo(naplesMap);
-  }
+    }).on('click', markerOnClick);
 
+    parksByCriteria[i].marker.addTo(naplesMap);
+  }
 }
 
 // Maybe do marker clusters: https://stackoverflow.com/questions/49333263/how-to-use-leaflet-markerclustergroup
